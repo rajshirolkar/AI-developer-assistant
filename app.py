fe0a60a7647d5b2fc88ad0c00a1f94b566e30f1d
Raj Shirolkar <rajshirolkar@gmail.com>
2023-12-04 18:27:37 -0800

    Created FastAPI instance for testing scores

--- app.py
+++ app.py
@@ -1,9 +1,76 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.params import Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import sqlite3
-from pydantic import BaseModel
-import uvicorn
+from pydantic import BaseModel, Field
+import openai
+import os
+from dotenv import load_dotenv
+
+from evaluation_utils.eval import evaluate_response
+
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+client = openai.Client()
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+class EvaluationScores(BaseModel):
+    Relevance: int = Field(..., description="Score for relevance of the text")
+    Coherence: int = Field(..., description="Score for coherence of the text")
+    Consistency: int = Field(..., description="Score for consistency of the text")
+    Fluency: int = Field(..., description="Score for fluency of the text")
+
+class GenerationResponse(BaseModel):
+    prompt: str = Field(..., description="The original prompt text")
+    response: str = Field(..., description="The generated text response from OpenAI")
+    scores: EvaluationScores = Field(..., description="Evaluation scores for the generated text")
+
+class Prompt(BaseModel):
+    text: str
+
+@app.get("/", response_class=HTMLResponse)
+async def read_form(request: Request):
+    return templates.TemplateResponse("input_form.html", {"request": request})
+
+
+@app.post("/generate-and-score/")
+async def generate_and_score(request: Request, prompt: str = Form(...)):
+    # Ensure the API key is set
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    print(request, prompt)
+
+    # Generate the response from OpenAI
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": prompt}]
+        )
+        generated_text = response.choices[0].message.content
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    # Evaluate the response (implement your evaluation logic here)
+    scores = evaluate_response(generated_text, client)
+
+    score_colors = {
+        "Relevance": "red" if int(scores['Relevance']) < 3 else "green",
+        "Coherence": "red" if int(scores['Coherence']) < 3 else "green",
+        "Consistency": "red" if int(scores['Consistency']) < 3 else "green",
+        "Fluency": "red" if int(scores['Fluency']) < 3 else "green",
+    }
+
+    response_data = {
+        "request": request,
+        "response": generated_text,
+        "scores": scores,
+        "score_colors": score_colors
+    }
+    return templates.TemplateResponse("input_form.html", response_data)
+
+
 
 class QueryInput(BaseModel):
     sql_query: str
@@ -36,4 +103,5 @@ def get_query_plan(query_input: QueryInput):
     return {"query_plan": plan_with_columns}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+    import uvicorn
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
---
+++ evaluation_utils/eval.py
@@ -0,0 +1,49 @@
+from evaluation_utils.eval_criteria import (
+    CONSISTENCY_SCORE_CRITERIA,
+    CONSISTENCY_SCORE_STEPS,
+    EVALUATION_PROMPT_TEMPLATE,
+    FLUENCY_SCORE_CRITERIA,
+    FLUENCY_SCORE_STEPS,
+    RELEVANCY_SCORE_CRITERIA,
+    RELEVANCY_SCORE_STEPS,
+    COHERENCE_SCORE_CRITERIA,
+    COHERENCE_SCORE_STEPS,
+)
+
+
+def evaluate_response(response_text, client):
+    evaluation_metrics = {
+        "Relevance": (RELEVANCY_SCORE_CRITERIA, RELEVANCY_SCORE_STEPS),
+        "Coherence": (COHERENCE_SCORE_CRITERIA, COHERENCE_SCORE_STEPS),
+        "Consistency": (CONSISTENCY_SCORE_CRITERIA, CONSISTENCY_SCORE_STEPS),
+        "Fluency": (FLUENCY_SCORE_CRITERIA, FLUENCY_SCORE_STEPS),
+    }
+
+    scores = {}
+    for eval_type, (criteria, steps) in evaluation_metrics.items():
+        score = get_geval_score(client, criteria, steps, response_text, eval_type)
+        scores[eval_type] = score.strip()
+
+    return scores
+
+def get_geval_score(client, criteria, steps, response, metric_name):
+    # Construct the evaluation prompt
+    prompt = EVALUATION_PROMPT_TEMPLATE.format(
+        criteria=criteria,
+        steps=steps,
+        document=response,  # In this case, the document is the response itself
+        summary=response,   # Using response as both the document and summary for evaluation
+        metric_name=metric_name,
+    )
+
+    # Call OpenAI API to evaluate
+    try:
+        api_response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": prompt}]
+        )
+        return api_response.choices[0].message.content
+    except Exception as e:
+        # Handle exceptions (e.g., API errors) and possibly log them
+        print(f"Error during evaluation: {e}")
+        return "0"  # Return a default score in case of an error
