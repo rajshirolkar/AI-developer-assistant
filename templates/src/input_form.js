027763d5ec72bb19353328e0a4f1f4ebfbd0f5a1
RuoluYe <yeruolu@uw.edu>
2024-02-03 15:17:17 -0800

    listener for toggle button

--- templates/src/input_form.js
+++ templates/src/input_form.js
@@ -1,8 +1,19 @@
-function toggle(name) {
-    var evalDiv = document.getElementById(name);
-    if (evalDiv.style.display === 'none') {
-        evalDiv.style.display = "block";
-    } else {
-        evalDiv.style.display = "none";
-    }
-}
+// event listeners
+document.addEventListener('DOMContentLoaded', () => {
+
+    // toggle buttons
+    document.querySelectorAll('.toggle-justification, .toggle-suggestion').forEach(button => {
+        button.addEventListener('click', function() {
+            // find the closest parent .eval-card
+            const parentCard = this.closest('.eval-card');
+            
+            // determine which class to toggle based on the clicked button
+            const classToToggle = this.classList.contains('toggle-justification')? '.justification' : 'suggestion'
+            const elementToToggle = parentCard.querySelector(classToToggle);
+            elementToToggle.style.display = elementToToggle.style.display === 'none' ? 'block' : 'none';
+        });
+    });
+
+    // thumbs=up preference
+    // todo #3
+})
