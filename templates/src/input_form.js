f88536aa4f5dcb5802a2fa5b2aee7dd9b4d2aca1
RuoluYe <yeruolu@uw.edu>
2024-02-04 01:54:31 -0800

    button toggle

--- templates/src/input_form.js
+++ templates/src/input_form.js
@@ -8,12 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const parentCard = this.closest('.eval-card');
             
             // determine which class to toggle based on the clicked button
-            const classToToggle = this.classList.contains('toggle-justification')? '.justification' : 'suggestion'
+            const classToToggle = this.classList.contains('toggle-justification')? '.justification' : '.suggestion'
             const elementToToggle = parentCard.querySelector(classToToggle);
             elementToToggle.style.display = elementToToggle.style.display === 'none' ? 'block' : 'none';
         });
     });
 
-    // thumbs=up preference
-    // todo #3
+    document.querySelectorAll('.thumb-up-button').forEach(button => {
+        button.addEventListener('click', function() {
+          // Toggle the "active" class each time the button is clicked
+          this.classList.toggle('active');
+        // thumbs=up preference
+        // todo #3
+        });
+    });
+
 })
